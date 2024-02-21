22d150f4fe68a4a8fd85140bb583471a27ad3321
Armando Lara <armandolarae97@gmail.com>
2024-02-21 15:28:00 -0600

    Update environment.development.ts

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    googleClientId: '144128334768-0e5t8amcf149tonit8bgc0sp99igisl4.apps.googleusercontent.com',
-    googleClientSecret: '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'
+    googleClientId: '',
+    googleClientSecret: ''
 };
